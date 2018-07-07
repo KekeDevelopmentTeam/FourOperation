4a93b2ee894fcc96cce59b2ccafbb8e75239d06e
silence3322 <40905688+silence3322@users.noreply.github.com>
2018-07-07 19:50:39 +0800

    刘浩提交

---
+++ QuestionBehavoir.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionBehavoir : MonoBehaviour {
+
+    // Use this for initialization
+    public Button button;
+    public int ans;
+    public InputField input;
+    public Text correctAns;
+	void Start () {
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ShowAnswer()
+    {
+        correctAns.text = "正确答案：" + ans.ToString();
+    }
+}
