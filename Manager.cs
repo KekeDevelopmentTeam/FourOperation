e1988c48e55b911bdab9aae2bb9c7d947335213e
HHDeng <497554102@qq.com>
2018-07-07 08:37:25 +0800

    FourOperation

---
+++ Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public GameObject questionPrefab;
+    public Dictionary<int, int> qustions;
+    public int count;
+    public static Manager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start () {
+		for(int i=0;i<count;i++)
+        {
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
